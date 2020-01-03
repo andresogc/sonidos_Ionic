068df59fb77f65a5af8ee1305bf3caf8995cc067
andresogc <andresogc2011@gmail.com>
2020-01-03 11:01:09 -0500

    Se crea la lista de animales en el home

---
+++ src/app/data/data.animales.ts
@@ -0,0 +1,46 @@
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+
+export const ANIMALES =[
+ {
+    nombre: "Caballo",
+    imagen: "assets/animales/caballo.png",
+    audio: "assets/sonidos/caballo.mp3",
+    duracion: 4,
+    reproduciendo: false
+},
+{
+    nombre: "Rinoceronte",
+    imagen: "assets/animales/rinoceronte.png",
+    audio: "assets/sonidos/rinoceronte.mp3",
+    duracion: 4,
+    reproduciendo: false
+},
+{
+    nombre: "Cerdo",
+    imagen: "assets/animales/cerdo.png",
+    audio: "assets/sonidos/cerdo.mp3",
+    duracion: 4,
+    reproduciendo: false
+},
+{
+    nombre: "Mono",
+    imagen: "assets/animales/mono.png",
+    audio: "assets/sonidos/mono.mp3",
+    duracion: 4,
+    reproduciendo: false
+},
+{
+    nombre: "Burro",
+    imagen: "assets/animales/burro.png",
+    audio: "assets/sonidos/burro.mp3",
+    duracion: 4,
+    reproduciendo: false
+},
+{
+    nombre: "Leon",
+    imagen: "assets/animales/leon.png",
+    audio: "assets/sonidos/leon.mp3",
+    duracion: 4,
+    reproduciendo: false
+},
+]
