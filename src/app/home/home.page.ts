0d6507fbef18b8b5215542a1af3d29f15fdfaf24
andresogc <andresogc2011@gmail.com>
2020-01-07 12:04:32 -0500

    Se agrego la fucnion de refresh y ordenar elementos de la lista animales

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ANIMALES } from "../data/data.animales";
 import { Animal } from "../../interfaces/animal.interface";
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,23 +11,71 @@ import { Animal } from "../../interfaces/animal.interface";
 export class HomePage {
 
   animales:Animal[] = [];
+  audio = new Audio;
+  audioTiempo:any;
+  ordenando:boolean = true; 
 
   constructor() {
 
-    this.animales = ANIMALES.splice(0);
+    this.animales = ANIMALES.slice(0);
   }
 
   reproducir (animal:Animal){
-    console.log(animal);
-    let audio = new Audio();
-    audio.src = animal.audio;
     
-    audio.load();
-    audio.play();
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+    console.log(animal);
+    
+    this.audio.src = animal.audio;
+    
+    this.audio.load();
+    this.audio.play();
 
     animal.reproduciendo = true;
-    setTimeout(() => animal.reproduciendo = false, animal.duracion*1000);
+
+    this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion*1000);
 
   }
 
+  private pausar_audio(animalSel:Animal){
+    clearTimeout(this.audioTiempo);
+
+    this.audio.pause();
+    this.audio.currentTime=0;
+
+    for (let animal of this.animales) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+      
+    }
+  }
+
+  borrar_animal(idx:number){
+    this.animales.splice(idx,1);
+  }
+  
+  recargar_animales(event){
+
+      
+    setTimeout( ()=> {
+      console.log("termino del refresh");
+      this.animales = ANIMALES.slice(0);
+    
+      event.target.complete();
+
+    }, 1000);
+
+  }
+
+
+  reorder (event){
+    console.log(event);
+   
+    event.detail.complete();
+  }
 }
